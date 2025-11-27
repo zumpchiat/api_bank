b3f8cb04493432df3076dc042544c8496af6c95c
zumpchiat <zumpchiat@gmail.com>
2025-11-27 15:39:17 -0300

    Revert "feat: config router in aplication users"
    This reverts commit d2611e4b41e8aade318ce4a4a6c78e611cdfef61.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from .configs.settings import settings
-from .routes import api_router
+from configs.settings import settings
+from routes import api_router
 
 app = FastAPI(title=settings.title)
 app.include_router(api_router)
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,3 @@
 from fastapi import APIRouter
 
-from .users.controller import router as user
 api_router = APIRouter()
-api_router.include_router(user, prefix="/usres", tags=["Users/"])
